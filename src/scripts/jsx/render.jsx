b469aaa80342698b404dfef121178a0bc224fd30
Rustie <64870518+woodcox@users.noreply.github.com>
2023-06-30 22:15:36 +0100

    Update render.jsx

--- src/scripts/jsx/render.jsx
+++ src/scripts/jsx/render.jsx
@@ -1,4 +1,4 @@
 import { render } from 'solid-js/web';
 import PdfmeGenerator from './pdf.jsx';
 
-render(() => <PdfmeGenerator />, document.getElementById('pdf'));
+render(() => <PdfmeGenerator />, document.getElementById('pdfapp'));
