f40678986bf4a504a42cf0e6ae46f3c22cbbdb12
Rustie <64870518+woodcox@users.noreply.github.com>
2023-12-06 16:45:30 +0000

    removed gzip again

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -15,11 +15,12 @@ const path = require("path");
 const esbuildOpts = {
   entryPoints: glob.sync(['src/scripts/jsx/render.jsx', 'src/scripts/js/*.js', 'dist/app/*.css']), // include css so that its in the manifest.json
   entryNames: isProd ? '[name]-[hash]' : '[name]',
+  // outExtension is currently disabled while this issue is resolved - https://github.com/jfortunato/esbuild-plugin-manifest/issues/18
   //outExtension: isProd ? {'.js': '.min.js', '.css': '.min.css'} : {'.js': '.js', '.css': '.css'},
   allowOverwrite: !isProd,  // overwrite dist/app/style.css when in dev mode
   bundle: true,
   minify: isProd,
-  write: !isProd,  // this is required for the gzipPlugin to work
+  //write: !isProd,  // this is required for the gzipPlugin to work
   treeShaking: isProd,
   outdir: './dist/app',
   sourcemap: !isProd,
@@ -53,13 +54,14 @@ const esbuildOpts = {
 }
 
 // If isProd include gzipPlugin. This is pushed into esBuildOpts.plugins because in dev/staging mode the esBuild's write api must be true. But the gzipPlugin requires it to be false.
-if (isProd) {
+/*if (isProd) {
   esbuildOpts.plugins.push(gzipPlugin({
     uncompressed: !isProd,
     gzip: isProd,
     brotli: isProd,
   }));
 }
+*/
 
 module.exports = async () => {
   let ctx = await esbuild.context({
