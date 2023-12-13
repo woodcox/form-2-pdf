9ee6ad5c8db0986949c1be1d9371843863bd2dbb
Rustie <64870518+woodcox@users.noreply.github.com>
2023-12-13 10:08:46 +0000

    update prod config to output min.js

--- config/build/esbuild.js
+++ config/build/esbuild.js
@@ -54,7 +54,7 @@ const esbuildOpts = {
 // If isProd include gzipPlugin. This is pushed into esBuildOpts.plugins because in dev/staging mode the esBuild's write api must be true. But the gzipPlugin requires it to be false.
 if (isProd) {
   esbuildOpts.plugins.push(gzipPlugin({
-    uncompressed: !isProd,
+    uncompressed: isProd,
     gzip: isProd,
     brotli: isProd,
   }));
