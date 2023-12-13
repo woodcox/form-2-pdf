1d85ffc4b418f8ffe397141cf299144b6ad503d0
Rustie <64870518+woodcox@users.noreply.github.com>
2023-12-13 17:38:38 +0000

    router

--- src/scripts/jsx/render.jsx
+++ src/scripts/jsx/render.jsx
@@ -1,5 +1,5 @@
 import { render } from 'solid-js/web';
-import { Router } from "@solidjs/router";
+import { Router, Route } from '@solidjs/router';
 import PdfmeGenerator from './pdfgen.jsx';
 import yourName from './yourName.jsx';
 import yourPartner from './yourPartner.jsx';
@@ -7,11 +7,9 @@ import yourPartner from './yourPartner.jsx';
 render(
     () => (
         <Router>
-            <Routes>
-                <Route path="/" component={PdfmeGenerator} /> {/* Define the home page route */}
-                <Route path="/name" component={yourName} /> 
-                <Route path="/partner" component={yourPartner} />
-            </Routes>
+            <Route path="/" component={PdfmeGenerator} /> {/* Define the home page route */}
+            <Route path="/name" component={yourName} /> 
+            <Route path="/partner" component={yourPartner} />
         </Router>
     ), 
     document.getElementById('pdfapp')
