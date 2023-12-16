853d5038ea723e31032a54af6816ea7498a3c02b
Rustie <64870518+woodcox@users.noreply.github.com>
2023-12-16 21:42:43 +0000

    test path prefix

--- src/scripts/jsx/render.jsx
+++ src/scripts/jsx/render.jsx
@@ -4,7 +4,9 @@ import PdfmeGenerator from './pdfgen.jsx';
 import yourName from './yourName.jsx';
 import yourPartner from './yourPartner.jsx';
 /* this accesses the environment variable PATHPREFIX set by the npm scripts (in the package.json) which is passed to solid-js by esbuild.js. Esbuild defines the environmental variables to pass through to solid-js app.*/
-const pathPrefix = process.env.PATHPREFIX;  
+const pathPrefix = process.env.PATHPREFIX;
+
+console.log('pathPrefix:', pathPrefix);
 
 render(
     () => (
