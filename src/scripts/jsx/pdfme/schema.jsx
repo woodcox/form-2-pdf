c7e92113f4448a8f7ef18be76e7a04a071fddb10
woodcox <64870518+woodcox@users.noreply.github.com>
2023-12-18 10:49:15 +0000

    restructured app folders

---
+++ src/scripts/jsx/pdfme/schema.jsx
@@ -0,0 +1,100 @@
+import { CEREMONY_PDF } from './basePdf.jsx';
+
+export const schema = { 
+  basePdf: CEREMONY_PDF,
+  schemas: [
+    {
+      YourFirstName: {
+        label: 'Your first name',
+        type: 'text',
+        position: { x: 68.28, y: 60.66 },
+        width: 100,
+        height: 10,
+        fieldType: 'text',
+        errormessage: 'Please enter your first name',
+        required: true,
+        autofocus: true,
+      },
+      YourSurname: {
+        label: 'Your surname',
+        type: 'text',
+        position: { x: 68.28, y: 60.66 },
+        width: 100,
+        height: 10,
+        fieldType: 'text',
+        errormessage: 'Please enter your surname',
+        required: true,
+        autofocus: true,
+      },
+      PartnerFirstName: {
+        label: 'Your partners first name',
+        type: 'text',
+        position: { x: 68.28, y: 60.66 },
+        width: 100,
+        height: 10,
+        fieldType: 'text',
+        errormessage: 'Please enter your partners first name',
+        required: true,
+        autofocus: true,
+      },
+      PartnerSurname: {
+        label: 'Your partners surname',
+        type: 'text',
+        position: { x: 68.28, y: 60.66 },
+        width: 100,
+        height: 10,
+        fieldType: 'text',
+        errormessage: 'Please enter your name',
+        required: true,
+        autofocus: true,
+      },
+      Email: {
+        label: 'Your email',
+        type: 'text',
+        position: { x: 52.4, y: 69.55 },
+        width: 140,
+        height: 10,
+        fieldType: 'text',
+        errormessage: 'Enter your full address',
+        required: false,
+      },
+      Phone: {
+        label: 'Your telephone number',
+        type: 'text',
+        position: { x: 53.76, y: 80.54 },
+        width: 100,
+        height: 10,
+        fieldType: 'tel',
+        errormessage: 'Enter a UK phone number',
+        autocomplete: 'tel',
+        required: true,
+      },
+      /* Date: {
+        label: 'Date of event',
+        type: 'text',
+        position: { x: 33.65, y: 104.63 },
+        width: 100,
+        height: 10,
+        fieldType: 'date',
+        required: true,
+        placeholder: 'dd/mm/yyyy',
+      }*/
+    },
+    // PAGE 2
+    // need new array for a new page
+    {
+      Mother: {
+        type: "text",
+        position: {
+          x: 101.34,
+          y: 30.69
+        },
+        width: 45,
+        height: 10,
+        rotate: 0,
+        alignment: "left",
+        verticalAlignment: "top"
+      }
+    }
+  ]
+}
--- src/scripts/jsx/render.jsx
+++ src/scripts/jsx/render.jsx
@@ -1,8 +1,8 @@
 import { render } from 'solid-js/web';
 import { Router, Route } from '@solidjs/router';
 import PdfmeGenerator from './pdfgen.jsx';
-import yourName from './yourName.jsx';
-import yourPartner from './yourPartner.jsx';
+import yourName from './pages/yourName.jsx';
+import yourPartner from './pages/yourPartner.jsx';
 /* this accesses the environment variable PATHPREFIX set by the npm scripts (in the package.json) which is passed to solid-js by esbuild.js. Esbuild defines the environmental variables to pass through to solid-js app.*/
 const pathPrefix = process.env.PATHPREFIX;
 const urlPrefix = pathPrefix ? `/${pathPrefix}` : "";
