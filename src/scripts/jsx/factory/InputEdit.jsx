77097277817435347b69b321a0ce0f6f3604829d
Rustie <64870518+woodcox@users.noreply.github.com>
2024-02-13 00:26:44 +0000

    Started Input with edit button component

---
+++ src/scripts/jsx/factory/InputEdit.jsx
@@ -0,0 +1,16 @@
+const InputEdit = (props) => {
+  
+    return (
+        <div class="inputEdit">
+            <label for={props.name}>{props.label}</label>
+            <input
+                name={props.name}
+                id={props.name}
+                value={props.value}
+            />
+            <button>{props.button}</button>
+        </div>
+    );
+};
+
+export default InputEdit;
