b718d3efa5c83ef032bb4bd8790accddd29e90f3
woodcox <64870518+woodcox@users.noreply.github.com>
2024-05-05 21:19:26 +0100

    govuk.js as seperate file

---
+++ src/scripts/js/gov-uk.js
@@ -0,0 +1,16 @@
+/*import { Radios } from 'govuk-frontend/dist/govuk/components/radios/radios.mjs';
+import { Button } from 'govuk-frontend/dist/govuk/components/button/button.mjs';
+
+// https://frontend.design-system.service.gov.uk/import-javascript/#import-individual-components
+const $radios = document.querySelectorAll('[data-module="govuk-radios"]')
+$radios.forEach(($radio) => {
+  new Radios($radio)
+});
+
+const $buttons = document.querySelectorAll('[data-module="govuk-button"]')
+$buttons.forEach(($button) => {
+  new Button($button)
+});
+
+*/
+
