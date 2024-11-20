7e2acfc8b66c3a2636a00d4a01a91c89b4f3151c
woodcox <git@stackblitz.com>
2024-11-20 17:20:21 +0000

    Conditional dead alive working

--- src/scripts/jsx/factory/AddAnotherParent.jsx
+++ src/scripts/jsx/factory/AddAnotherParent.jsx
@@ -1,5 +1,5 @@
 import { createSignal, For, Show, onCleanup, createEffect } from 'solid-js';
-import { createStore } from "solid-js/store"
+import { createStore } from 'solid-js/store';
 import { makePersisted } from './makePersisted.jsx';
 
 // Capitalise the first letter of a string. This is used in the AddAnother component to adjust the {prop.title}.
@@ -9,8 +9,6 @@ function capFirstLetter(string) {
 
 // Define a global object to store the state of all instances of AddAnotherParent
 const globalParentValues = {};
-
-
 
 function AddAnotherParent(props) {
   /* Conditionslly render if the first group of fields is visible based on the visible prop passed into the component.
@@ -22,52 +20,29 @@ function AddAnotherParent(props) {
   />
   */
 
-  // Initialize store
-const [parentInput, setParentInput] = createStore({
-  inputValues: [
-    {
-      id: 1,
-      fullName: "",
-      isAlive: "",
-      jobTitle: "",
-      isRetired: "",
-    },
-    {
-      id: 2,
-      fullName: "",
-      isAlive: "",
-      jobTitle: "",
-      isRetired: "",
-    }
-  ]
-})
+  const [parentInput, setParentInput] = createStore({
+    inputValues: props.visible
+      ? [{ id: 1, fullName: '', isAlive: '', jobTitle: '', isRetired: '' }]
+      : [],
+  });
 
-const updateField = (id, field, value) => {
-  setParentInput("inputValues", (item) => item.id === id, field, value);
-};
-
-  // Signal to render the number of parents to add
-  const [items, setItems] = createSignal(
-    props.visible
-      ? [{ id: 1, fields: { fullName: '', isAlive: '', jobTitle: '', isRetired: '' } }]
-      : []
-  );
   let nextId = 2;
-  // This is for setting focus to the new input fields when add (parent or step-parent) button is clicked
   let newInputRef = null;
 
-  // Only add a max of 2 parents
+  // Add a new parent
   const addItem = () => {
-    if (items().length < 2) {
-      setItems([
-        ...items(),
+    if (parentInput.inputValues.length < 2) {
+      setParentInput('inputValues', (prev) => [
+        ...prev,
         {
           id: nextId++,
-          fields: { fullName: '', isAlive: '', jobTitle: '', isRetired: '' },
+          fullName: '',
+          isAlive: '',
+          jobTitle: '',
+          isRetired: '',
         },
       ]);
 
-      // Set a timeout to focus the new fullName input when add button is clicked
       setTimeout(() => {
         if (newInputRef) {
           newInputRef.focus();
@@ -76,28 +51,26 @@ const updateField = (id, field, value) => {
     }
   };
 
+  // Remove a parent by ID
   const removeItem = (id) => {
-    setItems(items().filter((item) => item.id !== id));
-  };
-
-  const handleChange = (id, field, newValue) => {
-    setItems(
-      items().map((item) =>
-        item.id === id
-          ? { ...item, fields: { ...item.fields, [field]: newValue } }
-          : item
-      )
+    setParentInput('inputValues', (prev) =>
+      prev.filter((item) => item.id !== id)
     );
-    console.log('Updated items:', items());
   };
 
-  createEffect(() => {
-    console.log('Current state of items:', items());
-  });
+  // Handle field updates
+  const updateField = (id, field, value) => {
+    setParentInput('inputValues', (item) => item.id === id, field, value);
+  };
+
+  createEffect(
+    console.log('parent inputs:', parentInput)
+  );
+
 
   return (
     <div>
-      <For each={items()}>
+      <For each={parentInput.inputValues}>
         {(item, index) => (
           <div key={item.id} class="govuk-form-group">
             <h2>
@@ -106,96 +79,155 @@ const updateField = (id, field, value) => {
             <div id="parents-hint" class="govuk-hint">
               {props.hintText}
             </div>
+
+            {/* Full Name Input */}
             <div class="govuk-form-group">
               <label
                 for={`input-${item.id}-${props.title}-name`}
                 class="govuk-label"
               >
-                {capFirstLetter(props.title)} {index() + 1} Full Name
+                {capFirstLetter(props.title)} {index() + 1} full name
               </label>
               <input
                 id={`input-${item.id}-${props.title}-name`}
                 class="govuk-input"
                 type="text"
-                value={parentInput.inputValues.find((i) => i.id === item.id)?.fullName || ""}
-                ref={
-                  (el) => {
-                    if (index() === items().length - 1) newInputRef = el;
-                  } /* The field to focus on when the add button is clicked */
-                }
-                on:Change={(e) =>
-                  updateField(item.id, "fullName", e.target.value)}
+                value={item.fullName}
+                ref={(el) => {
+                  if (index() === parentInput.inputValues.length - 1) newInputRef = el;
+                }}
+                onChange={(e) => updateField(item.id, "fullName", e.target.value)}
               />
             </div>
+
+            {/* Is Alive Radio Buttons */}
             <div class="govuk-form-group">
               <fieldset class="govuk-fieldset">
                 <legend class="govuk-fieldset__legend govuk-fieldset__legend">
                   Is {props.title} {index() + 1} alive?
                 </legend>
-                <div class="govuk-radios" data-module="govuk-radios">
+                <div class="govuk-radios">
                   <div class="govuk-radios__item">
                     <input
                       class="govuk-radios__input"
                       type="radio"
                       name={`input-${item.id}-${props.title}-isAlive`}
-                      id={`input-${item.id}-${props.title}-isAlive`}
                       value="Yes, they are alive"
-                      //checked={item.fields.isAlive === 'Yes, they are alive'}
-                      on:Change={(e) =>
-                        updateField(item.id, "isAlive", e.target.value)}
+                      checked={item.isAlive === "Yes, they are alive"}
+                      onChange={(e) =>
+                        updateField(item.id, "isAlive", e.target.value)
+                      }
                     />
-                    <label
-                      class="govuk-label govuk-radios__label"
-                      for={`input-${item.id}-${props.title}-isAlive`}
-                    >
+                    <label class="govuk-label govuk-radios__label">
                       Yes, they are alive
                     </label>
                   </div>
                   <div class="govuk-radios__item">
                     <input
                       class="govuk-radios__input"
-                      id={`input-${item.id}-${props.title}-isAlive-2`}
-                      name={`input-${item.id}-${props.title}-isAlive`}
                       type="radio"
+                      name={`input-${item.id}-${props.title}-isAlive`}
                       value="No, they have died"
-                      //checked={item.fields.isAlive === 'No, they have died'}
-                      on:Change={(e) =>
+                      checked={item.isAlive === "No, they have died"}
+                      onChange={(e) =>
                         updateField(item.id, "isAlive", e.target.value)
                       }
                     />
-                    <label
-                      class="govuk-label govuk-radios__label"
-                      for={`input-${item.id}-${props.title}-isAlive-2`}
-                    >
+                    <label class="govuk-label govuk-radios__label">
                       No, they have died
                     </label>
                   </div>
                 </div>
               </fieldset>
             </div>
-            <Show
-              when={() => item.fields.isAlive === 'No, they have died'}
-              fallback={<div></div>}
-            >
+
+            {/* Job Title Input (Conditional === Died) */}
+            <Show when={item.isAlive === "No, they have died"}>
               <div class="govuk-form-group">
                 <label
                   for={`input-${item.id}-${props.title}-job`}
                   class="govuk-label"
                 >
-                  {`${capFirstLetter(props.title)} ${
-                    index() + 1
-                  }'s currrent or last job`}
+                  {capFirstLetter(props.title)} {index() + 1}'s last job
                 </label>
                 <input
                   id={`input-${item.id}-${props.title}-job`}
                   class="govuk-input"
                   type="text"
-                  value={parentInput.inputValues.find((i) => i.id === item.id)?.jobTitle || ""}
-                  on:Change={(e) =>
-                    updateField(item.id, "jobTitle", e.target.value)
-                  }
+                  value={item.jobTitle}
+                  onChange={(e) => updateField(item.id, "jobTitle", e.target.value)}
                 />
               </div>
+
+              {/* Retired radio buttons (Conditional === Died) */}
+              <div class="govuk-form-group">
+                <fieldset class="govuk-fieldset">
+                  <legend class="govuk-fieldset__legend govuk-fieldset__legend">
+                    {`Had ${props.title} ${index() + 1} retired before they died?`}
+                  </legend>
+                  <div class="govuk-radios" data-module="govuk-radios">
+                    <div class="govuk-radios__item">
+                      <input
+                        class="govuk-radios__input"
+                        type="radio"
+                        name={`input-${item.id}-${props.title}-isRetired`}
+                        id={`input-${item.id}-${props.title}-isRetired`}
+                        value="Yes"
+                        checked={item.isRetired === 'Yes'}
+                        onChange={(e) =>
+                          updateField(item.id, 'isRetired', e.target.value)
+                        }
+                      />
+                      <label
+                        class="govuk-label govuk-radios__label"
+                        for={`input-${item.id}-${props.title}-isRetired`}
+                      >
+                        Yes, they retired
+                      </label>
+                    </div>
+                    <div class="govuk-radios__item">
+                      <input
+                        class="govuk-radios__input"
+                        id={`input-${item.id}-${props.title}-isRetired-2`}
+                        name={`input-${item.id}-${props.title}-isRetired`}
+                        type="radio"
+                        value="No"
+                        checked={item.isRetired === 'No'}
+                        onChange={(e) =>
+                          updateField(item.id, 'isRetired', e.target.value)
+                        }
+                      />
+                      <label
+                        class="govuk-label govuk-radios__label"
+                        for={`input-${item.id}-${props.title}-isRetired-2`}
+                      >
+                        No
+                      </label>
+                    </div>
+                  </div>
+                </fieldset>
+              </div>
+            </Show>
+
+            {/* Job Title Input (Conditional === Alive) */}
+            <Show when={item.isAlive === "Yes, they are alive"}>
+              <div class="govuk-form-group">
+                <label
+                  for={`input-${item.id}-${props.title}-job`}
+                  class="govuk-label"
+                >
+                  {capFirstLetter(props.title)} {index() + 1}'s current or last job
+                </label>
+                <input
+                  id={`input-${item.id}-${props.title}-job`}
+                  class="govuk-input"
+                  type="text"
+                  value={item.jobTitle}
+                  onChange={(e) => updateField(item.id, "jobTitle", e.target.value)}
+                />
+              </div>
+
+              {/* Retired radio buttons (Conditional === Alive) */}
               <div class="govuk-form-group">
                 <fieldset class="govuk-fieldset">
                   <legend class="govuk-fieldset__legend govuk-fieldset__legend">
@@ -209,16 +241,16 @@ const updateField = (id, field, value) => {
                         name={`input-${item.id}-${props.title}-isRetired`}
                         id={`input-${item.id}-${props.title}-isRetired`}
                         value="Yes"
-                        //checked={item.fields.isRetired === 'Yes'}
-                        on:Change={(e) =>
-                          updateField(item.id, "isRetired", e.target.value)
+                        checked={item.isRetired === 'Yes'}
+                        onChange={(e) =>
+                          updateField(item.id, 'isRetired', e.target.value)
                         }
                       />
                       <label
                         class="govuk-label govuk-radios__label"
                         for={`input-${item.id}-${props.title}-isRetired`}
                       >
-                        Yes
+                        Yes, they retired
                       </label>
                     </div>
                     <div class="govuk-radios__item">
@@ -228,9 +260,10 @@ const updateField = (id, field, value) => {
                         name={`input-${item.id}-${props.title}-isRetired`}
                         type="radio"
                         value="No"
-                        //checked={item.fields.isRetired === 'No'}
-                        on:Change={(e) =>
-                          updateField(item.id, "isRetired", e.target.value)}
+                        checked={item.isRetired === 'No'}
+                        onChange={(e) =>
+                          updateField(item.id, 'isRetired', e.target.value)
+                        }
                       />
                       <label
                         class="govuk-label govuk-radios__label"
@@ -243,6 +276,8 @@ const updateField = (id, field, value) => {
                 </fieldset>
               </div>
             </Show>
+
+            {/* Remove Button */}
             <div class="govuk-button-group">
               <button
                 type="button"
@@ -255,8 +290,9 @@ const updateField = (id, field, value) => {
           </div>
         )}
       </For>
-      {/* Hide Add button if there are 2 parents already added */}
-      <Show when={items().length < 2}>
+
+      {/* Add Button */}
+      <Show when={parentInput.inputValues.length < 2}>
         <button type="button" class="govuk-button" onClick={addItem}>
           Add {props.title}
         </button>
